cb9ea2dfd0aa925b632456fa6bb86bdfc081278c
JhotamsDeveloper <jhotams.developer@gmail.com>
2023-05-22 02:11:00 -0500

    Se crea método AddInfrastructureEntityFrameworkServices

---
+++ MSschool.Infrastructure.EntityFramework/Infrastructure.EntityFramework.Registration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MSschool.Infrastructure.EntityFramework.Persistence;
+
+namespace MSschool.Infrastructure.EntityFramework;
+
+public static class Infrastructure
+{
+    public static IServiceCollection AddInfrastructureEntityFrameworkServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //Server=JHOTAMS;Database=MSSchoolS1;Integrated Security = true;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+        services.AddDbContext<MsschoolContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+        return services;
+    }
+}
