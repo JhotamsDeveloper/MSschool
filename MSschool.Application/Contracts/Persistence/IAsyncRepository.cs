db11f8f0a40d2b21b5cd3d2a64199ee11f98c227
JhotamsDeveloper <jhotams.developer@gmail.com>
2023-05-22 22:37:10 -0500

    Se crea el IAsyncRepository y IUnitOfWork

---
+++ MSschool.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+
+namespace MSschool.Application.Contracts.Persistence;
+
+public interface IAsyncRepository<T> where T : class
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+
+    Task<IReadOnlyList<T>> GetAsync(
+        Expression<Func<T, bool>>? predicate);
+
+    Task<IReadOnlyList<T>> GetAsync(
+        Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, 
+        string? includeString = null, bool? disableTracking = true);
+
+    Task<IReadOnlyList<T>> GetAsync(
+        Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, 
+        List<Expression<Func<T, object>>>? includes = null, 
+        bool? disableTracking = true);
+
+    Task<T> GetByIdAsync(Guid id);
+
+    Task<T> AddAsync(T entity);
+    
+    Task<T> UpdateAsync(T entity);
+    
+    Task DeleteAsync(T entity);
+
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ MSschool.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace MSschool.Application.Contracts.Persistence;
+
+public interface IUnitOfWork : IDisposable
+{
+    IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : class;
+    Task<int> Complete();
+}
