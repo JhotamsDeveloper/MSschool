ba54b19c37796575fedd9d6934c0ae5292c2e48b
JhotamsDeveloper <jhotams.developer@gmail.com>
2023-06-09 23:10:45 -0500

    Ajustes y configuraciones generales del proyecto

--- MSschool.Infrastructure.EntityFramework/Persistence/MsschoolContext.cs
+++ MSschool.Infrastructure.EntityFramework/Persistence/MsschoolContext.cs
@@ -2,31 +2,30 @@
 using MSschool.Application.Domain.Models.AcademicLevels;
 using MSschool.Application.Domain.Models.AcademicProgramUsers;
 using MSschool.Application.Domain.Models.AcademicsProgram;
+using MSschool.Application.Domain.Models.Categories;
 using MSschool.Application.Domain.Models.CourseNotes;
 using MSschool.Application.Domain.Models.Institutions;
 using MSschool.Application.Domain.Models.Schedules;
 using MSschool.Application.Domain.Models.Subjects;
 using MSschool.Application.Domain.Models.SubjectsAcademicProgram;
-using MSschool.Application.Domain.Models.Users;
 using MSschool.Application.Domain.Models.UserAssignments;
-using MSschool.Application.Domain.Models.ValueOfTheSubjects;
-using MSschool.Infrastructure.EntityFramework.Configurations;
 using MSschool.Application.Domain.Models.UserCategories;
-using MSschool.Application.Domain.Models.Categories;
+using MSschool.Application.Domain.Models.Users;
+using MSschool.Application.Domain.Models.ValueOfTheSubjects;
 using System.Reflection;
 
 namespace MSschool.Infrastructure.EntityFramework.Persistence;
 
-public partial class MsschoolContext: DbContext
+public partial class MsschoolContext : DbContext
 {
     public MsschoolContext()
     {
-            
+
     }
 
-    public MsschoolContext(DbContextOptions<MsschoolContext> options): base(options)
+    public MsschoolContext(DbContextOptions<MsschoolContext> options) : base(options)
     {
-        
+
     }
 
     public virtual DbSet<AcademicLevel> AcademicLevels { get; set; }
@@ -64,7 +63,7 @@ public partial class MsschoolContext: DbContext
         modelBuilder.Entity<Category>()
             .HasData(MsschoolContextSeed
             .PreloadedCategories());
-        
+
         OnModelCreatingPartial(modelBuilder);
     }
 
