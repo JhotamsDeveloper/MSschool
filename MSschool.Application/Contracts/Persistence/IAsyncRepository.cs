a9daf1b49a82891ddeec6b9dc59776237227e1c9
JhotamsDeveloper <jhotams.developer@gmail.com>
2023-07-17 10:01:24 -0500

    Fix

--- MSschool.Application/Contracts/Persistence/IAsyncRepository.cs
+++ MSschool.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -7,36 +7,25 @@ public interface IAsyncRepository<T> where T : class
 {
     Task<IReadOnlyList<T>> GetAllAsync();
     Task<IReadOnlyList<T>> GetAllIgnoreQueryFiltersAsync();
-
     Task<IReadOnlyList<T>> GetAsync(
         Expression<Func<T, bool>>? predicate);
-
     Task<IReadOnlyList<T>> GetAsync(
         Expression<Func<T, bool>>? predicate = null,
         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, 
         string? includeString = null, bool? disableTracking = true);
-
     Task<IReadOnlyList<T>> GetAsync(
         Expression<Func<T, bool>>? predicate = null,
         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, 
         List<Expression<Func<T, object>>>? includes = null, 
         bool? disableTracking = true);
-
     Task<int> CountAsync(ISpecification<T> spec);
-
     Task<T> GetIdWithSpec(ISpecification<T> spec);
     Task<IReadOnlyList<T>> GetAllWithSpec(ISpecification<T> spec);
-
     Task<bool> Exitst(Expression<Func<T, bool>> predicate);
-
     Task<T> GetByIdAsync(Guid id);
-
     Task AddAsync(T entity);
-    
     Task UpdateAsync(T entity);
-    
     Task DeleteAsync(T entity);
-
     void Add(T entity);
     void Update(T entity);
     void Delete(T entity);
